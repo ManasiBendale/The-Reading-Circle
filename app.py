027450eeee04d32ca42e855dc9ba0fb01465b547
anittala <44583555+anittala@users.noreply.github.com>
2023-03-08 20:31:56 -0800

    Updated app.py

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:Sanjose$2023@flaskdb.cviupmaskxl1.us-east-1.rds.amazonaws.com:3306/flaskaws'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/flaskaws'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "somethingunique"
 
+engine = create_engine('mysql://root:''@localhost/flaskaws')
+connection = engine.raw_connection()
+# cursor = connection.cursor()
 db = SQLAlchemy(app)
+
+# db = MySQL(app)
 
 
 class Book(db.Model):
@@ -22,19 +32,113 @@ class Book(db.Model):
         self.price = price
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    participation = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, username, email, password, participation):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.participation = participation
+
+
 @app.route('/')
 def index():
     books = Book.query.all()
     return render_template('index.html', books=books)
 
 
-@app.route('/add/', methods =['POST'])
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form and 'participation' in request.form:
+        # user = User(
+        # username=request.form.get('username'),
+        # password=request.form.get('password'),
+        # email=request.form.get('email'),
+        # participation=request.form.get('participation')
+        # )
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        participation = request.form['participation']
+        password = sha256_crypt.encrypt(str(password))
+
+        cursor = connection.cursor(MySQLdb.cursors.DictCursor)
+        userdata = cursor.execute(
+            'SELECT * FROM User WHERE username = % s', [username, ])
+        print("ACCOUNT:", userdata)
+        # account = db.session.execute(
+        #     db.select(User).filter_by(username=username)).scalar_one()
+        # account = str(userdata).fetchone()
+        account = userdata
+        print("ACCOUNT:", type(account))
+        if account:
+            flash('Account already exists! Retry.')
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            flash('Invalid email address!')
+
+        else:
+            user = User(username=username, email=email,
+                        password=password, participation=participation)
+            db.session.add(user)
+            db.session.commit()
+            flash("User added successfully")
+            # msg = 'You have successfully registered !'
+        # db.session.add(user)
+        # db.session.commit()
+
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    # return render_template('register.html', msg=msg)
+    return redirect(url_for('index'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        # cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor = connection.cursor(MySQLdb.cursors.DictCursor)
+        account = cursor.execute(
+            'SELECT * FROM User WHERE username = % s AND password = % s', [username, password, ])
+        # account = cursor.fetchone()
+        print("QWERTYUIOP", account)
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            flash("User logged in successfully")
+            return render_template('index.html', msg=msg)
+        else:
+            flash('Incorrect username / password !')
+    # return render_template('login.html', msg=msg)
+
+    return redirect(url_for('index'))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    flash("User logged out successfully")
+    return redirect(url_for('index'))
+
+
+@app.route('/add/', methods=['POST'])
 def insert_book():
     if request.method == "POST":
         book = Book(
-            title = request.form.get('title'),
-            author = request.form.get('author'),
-            price = request.form.get('price')
+            title=request.form.get('title'),
+            author=request.form.get('author'),
+            price=request.form.get('price')
         )
         db.session.add(book)
         db.session.commit()
@@ -42,7 +146,7 @@ def insert_book():
         return redirect(url_for('index'))
 
 
-@app.route('/update/', methods = ['POST'])
+@app.route('/update/', methods=['POST'])
 def update():
     if request.method == "POST":
         my_data = Book.query.get(request.form.get('id'))
@@ -56,7 +160,7 @@ def update():
         return redirect(url_for('index'))
 
 
-@app.route('/delete/<id>/', methods = ['GET', 'POST'])
+@app.route('/delete/<id>/', methods=['GET', 'POST'])
 def delete(id):
     my_data = Book.query.get(id)
     db.session.delete(my_data)
