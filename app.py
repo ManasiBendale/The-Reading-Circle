7e12136fb3a8f2fb0ae62e8a9f7298c934bc6a06
shubhamshinde245 <shubham.shinde245@gmail.com>
2023-03-11 14:18:18 -0800

    DB Connection attempt

--- app.py
+++ app.py
@@ -8,18 +8,15 @@ from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 
-# app.jinja_env.filters['zip'] = zip
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = ''
-# app.config['MYSQL_DB'] = 'test1'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/test1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:<password>@localhost:3306/<database_name>'
 
-app.config['MYSQL_HOST'] = 'flaskdb.cviupmaskxl1.us-east-1.rds.amazonaws.com:3306'
-app.config['MYSQL_USER'] = 'admin'
-app.config['MYSQL_PASSWORD'] = 'admin1234'
-app.config['MYSQL_DB'] = 'flaskaws'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:admin1234@swaptable.cgurkhcysih2.us-west-2.rds.amazonaws.com/swaptable'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://'
+app.config['SQLALCHEMY_USER'] = 'root'
+app.config['SQLALCHEMY_PASSWORD'] = '<password>'
+app.config['SQLALCHEMY_HOST'] = 'localhost'
+app.config['SQLALCHEMY_PORT'] = 3306
+app.config['SQLALCHEMY_DATABASE_NAME'] = '<database_name>'
+
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "somethingunique"
@@ -230,41 +227,41 @@ def choose_book():
         flash("Book chosen successfully!", "success")
         return redirect(url_for('index'))
 
-@app.route('/Swap Books/', methods=['POST'])
+@app.route('/SWAP/', methods=['POST'])
 def swap_table():
-    import random
+    if request.method == "POST":
+        import random
 
-    def shuffle_records(records):
-        shuffled_records = random.sample(records, len(records))
-        return shuffled_records
+        def shuffle_records(records):
+            shuffled_records = random.sample(records, len(records))
+            return shuffled_records
 
-    def book_swap(l):
-        records = [(record[0], record[3], record[5], record[2]) for record in l]
-        shuffled = shuffle_records(records)
-        swapped = set()
-        for i, record in enumerate(l):
-            while (record[0], shuffled[i][1]) in swapped or shuffled[i][1] == record[2] or shuffled[i][3] == record[3]:
-                random.shuffle(shuffled)
-            record[3] = shuffled[i][1]
-            swapped.add((record[0], record[3]))
-            print(f"Swapped {record[0]} with {record[3]}")
-        print("\n")
-        return l
+        def book_swap(l):
+            records = [(record[0], record[3], record[5], record[2]) for record in l]
+            shuffled = shuffle_records(records)
+            swapped = set()
+            for i, record in enumerate(l):
+                while (record[0], shuffled[i][1]) in swapped or shuffled[i][1] == record[2] or shuffled[i][3] == record[3]:
+                    random.shuffle(shuffled)
+                record[3] = shuffled[i][1]
+                swapped.add((record[0], record[3]))
+                print(f"Swapped {record[0]} with {record[3]}")
+            print("\n")
+            return l
 
-    l = [["ID 1" , "Title 1", "Main Owner 1", "New Owner 1", "BookId 1", "UserId 1"],
-         ["ID 2" , "Title 2", "Main Owner 2", "New Owner 2", "BookId 2", "UserId 2"],
-         ["ID 3" , "Title 3", "Main Owner 3", "New Owner 3", "BookId 3", "UserId 3"],
-         ["ID 4" , "Title 4", "Main Owner 4", "New Owner 4", "BookId 4", "UserId 4"],
-         ["ID 5" , "Title 5", "Main Owner 5", "New Owner 5", "BookId 5", "UserId 5"],
-         ["ID 6" , "Title 6", "Main Owner 6", "New Owner 6", "BookId 6", "UserId 6"]]
+        l = [["ID 1" , "Title 1", "Main Owner 1", "New Owner 1", "BookId 1", "UserId 1"],
+            ["ID 2" , "Title 2", "Main Owner 2", "New Owner 2", "BookId 2", "UserId 2"],
+            ["ID 3" , "Title 3", "Main Owner 3", "New Owner 3", "BookId 3", "UserId 3"],
+            ["ID 4" , "Title 4", "Main Owner 4", "New Owner 4", "BookId 4", "UserId 4"],
+            ["ID 5" , "Title 5", "Main Owner 5", "New Owner 5", "BookId 5", "UserId 5"],
+            ["ID 6" , "Title 6", "Main Owner 6", "New Owner 6", "BookId 6", "UserId 6"]]
 
-    swapped_books = book_swap(l)
-    for book in swapped_books:
-        print(book)
+        swapped_books = book_swap(l)
+        for book in swapped_books:
+            print(book)
 
-    if len(set((book[0], book[3], book[2]) for book in swapped_books)) == len(swapped_books):
-        print("\n All records have been swapped.")
-
+        if len(set((book[0], book[3], book[2]) for book in swapped_books)) == len(swapped_books):
+            print("\n All records have been swapped.")
 
 if __name__ == "__main__":
     app.run(debug=True)
